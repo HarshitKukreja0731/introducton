d0816b9f620ac980d45b0c96d043856a98eb2491
HarshitKukreja0731 <122436300+HarshitKukreja0731@users.noreply.github.com>
2023-03-10 13:47:49 +0530

    Add files via upload

---
+++ arraysort.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+    int n,i,j,temp;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++){
+        for(j=i;j<n;j++){
+            if(arr[i]>arr[j]){
+                temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+            else
+                continue;
+        }
+    }
+    for(i=0;i<n;i++){
+        printf("%d ",arr[i]);
+    }
+    return 0;
+}
+
