b5be4b0e8d52d9b8bf0bc3ba8ea9ef0bc8557190
Harshit <harshit387.be22@chitkara.edu.in>
2023-03-10 14:02:28 +0530

    changes from d

--- arraysort.c
+++ arraysort.c
@@ -4,7 +4,11 @@ int main()
     int n,i,j,temp;
     scanf("%d",&n);
     int arr[n];
+<<<<<<< HEAD
     printf("this program sorts the array");
+=======
+    printf("this s the array sorting program");
+>>>>>>> 8dcfcf0 (changes from d)
     for(i=0;i<n;i++){
         scanf("%d",&arr[i]);
     }
